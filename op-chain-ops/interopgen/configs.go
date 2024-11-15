defitricks/optimism

opcm-redesign: op-deployer changes

op-chain-ops/interopgen/configs.go
@@ -35,7 +35,7 @@ type SuperFaultProofConfig struct {
 }
 
 type OPCMImplementationsConfig struct {
-	Release string
+	L1ContractsReleaseVersion string
 
 	FaultProof SuperFaultProofConfig
 

op-chain-ops/interopgen/deploy.go
@@ -170,10 +170,9 @@ func DeploySuperchainToL1(l1Host *script.Host, superCfg *SuperchainConfig) (*Sup
 		ProofMaturityDelaySeconds:       superCfg.Implementations.FaultProof.ProofMaturityDelaySeconds,
 		DisputeGameFinalityDelaySeconds: superCfg.Implementations.FaultProof.DisputeGameFinalityDelaySeconds,
 		MipsVersion:                     superCfg.Implementations.FaultProof.MipsVersion,
-		Release:                         superCfg.Implementations.Release,
+		L1ContractsReleaseVersion:       superCfg.Implementations.L1ContractsReleaseVersion,
 		SuperchainConfigProxy:           superDeployment.SuperchainConfigProxy,
 		ProtocolVersionsProxy:           superDeployment.ProtocolVersionsProxy,
-		OpcmProxyOwner:                  superDeployment.SuperchainProxyAdmin,
 		UseInterop:                      superCfg.Implementations.UseInterop,
 		StandardVersionsToml:            standard.VersionsMainnetData,
 	})
@@ -210,7 +209,7 @@ func DeployL2ToL1(l1Host *script.Host, superCfg *SuperchainConfig, superDeployme
 		BasefeeScalar:           cfg.GasPriceOracleBaseFeeScalar,
 		BlobBaseFeeScalar:       cfg.GasPriceOracleBlobBaseFeeScalar,
 		L2ChainId:               new(big.Int).SetUint64(cfg.L2ChainID),
-		OpcmProxy:               superDeployment.OpcmProxy,
+		Opcm:                    superDeployment.Opcm,
 		SaltMixer:               cfg.SaltMixer,
 		GasLimit:                cfg.GasLimit,
 		DisputeGameType:         cfg.DisputeGameType,

op-deployer/pkg/deployer/pipeline/implementations.go
@@ -70,10 +70,9 @@ func DeployImplementations(env *Env, intent *state.Intent, st *state.State) erro
 			ProofMaturityDelaySeconds:       new(big.Int).SetUint64(proofParams.ProofMaturityDelaySeconds),
 			DisputeGameFinalityDelaySeconds: new(big.Int).SetUint64(proofParams.DisputeGameFinalityDelaySeconds),
 			MipsVersion:                     new(big.Int).SetUint64(proofParams.MIPSVersion),
-			Release:                         contractsRelease,
+			L1ContractsReleaseVersion:       contractsRelease,
 			SuperchainConfigProxy:           st.SuperchainDeployment.SuperchainConfigProxyAddress,
 			ProtocolVersionsProxy:           st.SuperchainDeployment.ProtocolVersionsProxyAddress,
-			OpcmProxyOwner:                  st.SuperchainDeployment.ProxyAdminAddress,
 			StandardVersionsToml:            standardVersionsTOML,
 			UseInterop:                      intent.UseInterop,
 		},
@@ -83,7 +82,7 @@ func DeployImplementations(env *Env, intent *state.Intent, st *state.State) erro
 	}
 
 	st.ImplementationsDeployment = &state.ImplementationsDeployment{
-		OpcmProxyAddress:                        dio.OpcmProxy,
+		OpcmAddress:                             dio.Opcm,
 		DelayedWETHImplAddress:                  dio.DelayedWETHImpl,
 		OptimismPortalImplAddress:               dio.OptimismPortalImpl,
 		PreimageOracleSingletonAddress:          dio.PreimageOracleSingleton,

ethereum-optimism/optimism-monorepo

op-chain-ops/interopgen/configs.go
@@ -35,7 +35,7 @@ type SuperFaultProofConfig struct {
 }
 
 type OPCMImplementationsConfig struct {
-	Release string
+	L1ContractsRelease string
 
 	FaultProof SuperFaultProofConfig
 

op-chain-ops/interopgen/deploy.go
@@ -170,10 +170,9 @@ func DeploySuperchainToL1(l1Host *script.Host, superCfg *SuperchainConfig) (*Sup
 		ProofMaturityDelaySeconds:       superCfg.Implementations.FaultProof.ProofMaturityDelaySeconds,
 		DisputeGameFinalityDelaySeconds: superCfg.Implementations.FaultProof.DisputeGameFinalityDelaySeconds,
 		MipsVersion:                     superCfg.Implementations.FaultProof.MipsVersion,
-		Release:                         superCfg.Implementations.Release,
+		L1ContractsRelease:              superCfg.Implementations.L1ContractsRelease,
 		SuperchainConfigProxy:           superDeployment.SuperchainConfigProxy,
 		ProtocolVersionsProxy:           superDeployment.ProtocolVersionsProxy,
-		OpcmProxyOwner:                  superDeployment.SuperchainProxyAdmin,
 		UseInterop:                      superCfg.Implementations.UseInterop,
 		StandardVersionsToml:            standard.VersionsMainnetData,
 	})
@@ -210,7 +209,7 @@ func DeployL2ToL1(l1Host *script.Host, superCfg *SuperchainConfig, superDeployme
 		BasefeeScalar:           cfg.GasPriceOracleBaseFeeScalar,
 		BlobBaseFeeScalar:       cfg.GasPriceOracleBlobBaseFeeScalar,
 		L2ChainId:               new(big.Int).SetUint64(cfg.L2ChainID),
-		OpcmProxy:               superDeployment.OpcmProxy,
+		Opcm:                    superDeployment.Opcm,
 		SaltMixer:               cfg.SaltMixer,
 		GasLimit:                cfg.GasLimit,
 		DisputeGameType:         cfg.DisputeGameType,

op-deployer/pkg/deployer/pipeline/implementations.go
@@ -68,10 +68,9 @@ func DeployImplementations(env *Env, intent *state.Intent, st *state.State) erro
 			ProofMaturityDelaySeconds:       new(big.Int).SetUint64(proofParams.ProofMaturityDelaySeconds),
 			DisputeGameFinalityDelaySeconds: new(big.Int).SetUint64(proofParams.DisputeGameFinalityDelaySeconds),
 			MipsVersion:                     new(big.Int).SetUint64(proofParams.MIPSVersion),
-			Release:                         contractsRelease,
+			L1ContractsRelease:              contractsRelease,
 			SuperchainConfigProxy:           st.SuperchainDeployment.SuperchainConfigProxyAddress,
 			ProtocolVersionsProxy:           st.SuperchainDeployment.ProtocolVersionsProxyAddress,
-			OpcmProxyOwner:                  st.SuperchainDeployment.ProxyAdminAddress,
 			StandardVersionsToml:            standardVersionsTOML,
 			UseInterop:                      intent.UseInterop,
 		},
@@ -81,7 +80,7 @@ func DeployImplementations(env *Env, intent *state.Intent, st *state.State) erro
 	}
 
 	st.ImplementationsDeployment = &state.ImplementationsDeployment{
-		OpcmProxyAddress:                        dio.OpcmProxy,
+		OpcmAddress:                             dio.Opcm,
 		DelayedWETHImplAddress:                  dio.DelayedWETHImpl,
 		OptimismPortalImplAddress:               dio.OptimismPortalImpl,
 		PreimageOracleSingletonAddress:          dio.PreimageOracleSingleton,