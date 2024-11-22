552a5a92afa35b02f95b9d6fd89cd8f81b6e5d5c
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-11-22 13:16:54 -0500

    op-deployer: add unit test for inent.validateStandardValues

---
+++ op-deployer/pkg/deployer/state/intent_test.go
@@ -0,0 +1,53 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateStandardValues(t *testing.T) {
+	intent := initStandardIntent()
+	err := intent.ValidateIntentConfigType()
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrChainRoleZeroAddress)
+
+	setChainRoles(intent)
+	err = intent.ValidateIntentConfigType()
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrFeeVaultZeroAddress)
+
+	setFeeAddresses(intent)
+	err = intent.ValidateIntentConfigType()
+	require.NoError(t, err)
+
+	intent.Chains[0].Eip1559Denominator = 3
+	err = intent.ValidateIntentConfigType()
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrNonStandardValue)
+}
+
+func initStandardIntent() *Intent {
+	intent := Intent{
+		L1ChainID:        1,
+		IntentConfigType: IntentConfigTypeStandard,
+	}
+	intent.setStandardValues([]common.Hash{common.HexToHash("0x336")})
+	return &intent
+}
+
+func setChainRoles(intent *Intent) {
+	intent.Chains[0].Roles.L1ProxyAdminOwner = common.HexToAddress("0x01")
+	intent.Chains[0].Roles.L2ProxyAdminOwner = common.HexToAddress("0x01")
+	intent.Chains[0].Roles.SystemConfigOwner = common.HexToAddress("0x01")
+	intent.Chains[0].Roles.UnsafeBlockSigner = common.HexToAddress("0x01")
+	intent.Chains[0].Roles.Batcher = common.HexToAddress("0x01")
+	intent.Chains[0].Roles.Proposer = common.HexToAddress("0x01")
+}
+
+func setFeeAddresses(intent *Intent) {
+	intent.Chains[0].BaseFeeVaultRecipient = common.HexToAddress("0x08")
+	intent.Chains[0].L1FeeVaultRecipient = common.HexToAddress("0x09")
+	intent.Chains[0].SequencerFeeVaultRecipient = common.HexToAddress("0x0A")
+}
