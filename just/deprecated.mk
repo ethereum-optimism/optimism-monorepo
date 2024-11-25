c019aeb010fde2183b6df37897dbcf690227cd7a
Yann Hodique <yann@oplabs.co>
2024-11-25 14:50:10 +0100

    fix(just): parallel support
    For some unclear reason runs in CI environment are invalid (they
    generate some "empty" calls to the private target).
    Sidestep the issue for now.

--- just/deprecated.mk
+++ just/deprecated.mk
@@ -1,6 +1,9 @@
 ifeq (, $(shell which tput))
   # CI environment typically does not support tput.
   banner-style = $1
+else ifeq (, $(TERM))
+  # Terminal type not set, so tput would fail.
+  banner-style = $1
 else
   # print in bold red to bring attention.
   banner-style = $(shell tput bold)$(shell tput setaf 1)$1$(shell tput sgr0)
@@ -12,7 +15,8 @@ include $(SELF_DIR)/flags.mk
 define make-deprecated-target
 $1:
 	@echo
-	@printf %s\\n '$(call banner-style,"make $1 $(JUSTFLAGS)" is deprecated. Please use "just $(JUSTFLAGS) $1" instead.)'
+	@printf %s\\n '$(call banner-style,Deprecated make call: make $1 $(JUSTFLAGS))'
+	@printf %s\\n '$(call banner-style,Consider using just instead: just $(JUSTFLAGS) $1)'
 	@echo
 	just $(JUSTFLAGS) $1
 endef
