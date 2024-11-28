4a45ed2f905bd56d5e31fdf4e5188ac3a25a2619
agusduha <agusnduha@gmail.com>
2024-11-28 17:47:20 -0300

    fix: pre pr

--- packages/contracts-bedrock/src/L1/SuperchainConfig.sol
+++ packages/contracts-bedrock/src/L1/SuperchainConfig.sol
@@ -62,8 +62,8 @@ contract SuperchainConfig is Initializable, ISemver {
     error ChainAlreadyAdded();
 
     /// @notice Semantic version.
-    /// @custom:semver 1.1.1-beta.2
-    string public constant version = "1.1.1-beta.2";
+    /// @custom:semver 1.1.1-beta.3
+    string public constant version = "1.1.1-beta.3";
 
     // Mapping from chainId to SystemConfig address
     mapping(uint256 => address) public systemConfigs;
