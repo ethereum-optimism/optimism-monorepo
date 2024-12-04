2e25060c1ce68b4f59135dd348fc00929c4656dd
Joonkyo Kim <rootwarp@gmail.com>
2024-12-04 18:42:19 +0900

    op-proposer: add stopped flag

--- op-proposer/proposer/config.go
+++ op-proposer/proposer/config.go
@@ -59,6 +59,10 @@ type CLIConfig struct {
 	// ActiveSequencerCheckDuration is the duration between checks to determine the active sequencer endpoint.
 	ActiveSequencerCheckDuration time.Duration
 
+	// If Stopped is true, the proposer starts stopped and won't start proposing right away.
+	// Proposing needs to be started via an admin RPC.
+	Stopped bool
+
 	// Whether to wait for the sequencer to sync to a recent block at startup.
 	WaitNodeSync bool
 }
