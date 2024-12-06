855a0a7e01b7b21bd14ab780362b3ba776fc3cbe
Matthew Slipper <me@matthewslipper.com>
2024-12-05 20:29:46 -0700

    code review updates

--- op-deployer/pkg/deployer/opcm/proxy.go
+++ op-deployer/pkg/deployer/opcm/proxy.go
@@ -1,8 +1,6 @@
 package opcm
 
 import (
-	"fmt"
-
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/ethereum-optimism/optimism/op-chain-ops/script"
@@ -20,13 +18,6 @@ type DeployProxyOutput struct {
 	Proxy common.Address
 }
 
-func (output *DeployProxyOutput) CheckOutput(input common.Address) error {
-	if output.Proxy == (common.Address{}) {
-		return fmt.Errorf("output.Proxy not set")
-	}
-	return nil
-}
-
 type DeployProxyScript struct {
 	Run func(input, output common.Address) error
 }
@@ -35,33 +26,5 @@ func DeployProxy(
 	host *script.Host,
 	input DeployProxyInput,
 ) (DeployProxyOutput, error) {
-	var output DeployProxyOutput
-	inputAddr := host.NewScriptAddress()
-	outputAddr := host.NewScriptAddress()
-
-	cleanupInput, err := script.WithPrecompileAtAddress[*DeployProxyInput](host, inputAddr, &input)
-	if err != nil {
-		return output, fmt.Errorf("failed to insert DeployProxyInput precompile: %w", err)
-	}
-	defer cleanupInput()
-
-	cleanupOutput, err := script.WithPrecompileAtAddress[*DeployProxyOutput](host, outputAddr, &output,
-		script.WithFieldSetter[*DeployProxyOutput])
-	if err != nil {
-		return output, fmt.Errorf("failed to insert DeployProxyOutput precompile: %w", err)
-	}
-	defer cleanupOutput()
-
-	implContract := "DeployProxy"
-	deployScript, cleanupDeploy, err := script.WithScript[DeployProxyScript](host, "DeployProxy.s.sol", implContract)
-	if err != nil {
-		return output, fmt.Errorf("failed to load %s script: %w", implContract, err)
-	}
-	defer cleanupDeploy()
-
-	if err := deployScript.Run(inputAddr, outputAddr); err != nil {
-		return output, fmt.Errorf("failed to run %s script: %w", implContract, err)
-	}
-
-	return output, nil
+	return RunBasicScript[DeployProxyInput, DeployProxyOutput](host, input, "DeployProxy.s.sol", "DeployProxy")
 }
--- packages/contracts-bedrock/scripts/deploy/DeployProxy.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployProxy.s.sol
@@ -9,7 +9,7 @@ import { BaseDeployIO } from "scripts/deploy/BaseDeployIO.sol";
 import { DeployUtils } from "scripts/libraries/DeployUtils.sol";
 
 // Interfaces
-import { IProxy } from "src/universal/interfaces/IProxy.sol";
+import { IProxy } from "interfaces/universal/IProxy.sol";
 
 /// @title DeployProxyInput
 contract DeployProxyInput is BaseDeployIO {
@@ -44,39 +44,20 @@ contract DeployProxyOutput is BaseDeployIO {
         }
     }
 
-    function checkOutput(DeployProxyInput _mi) public {
-        DeployUtils.assertValidContractAddress(address(_proxy));
-        assertValidDeploy(_mi);
-    }
-
     function proxy() public view returns (IProxy) {
         DeployUtils.assertValidContractAddress(address(_proxy));
         return _proxy;
-    }
-
-    function assertValidDeploy(DeployProxyInput _mi) public {
-        assertValidProxy(_mi);
-    }
-
-    function assertValidProxy(DeployProxyInput _mi) internal {
-        IProxy prox = proxy();
-        vm.prank(_mi.owner());
-        address proxyOwner = prox.admin();
-
-        require(
-            proxyOwner == _mi.owner(), "DeployProxy: owner of proxy does not match the owner specified in the input"
-        );
     }
 }
 
 /// @title DeployProxy
 contract DeployProxy is Script {
     function run(DeployProxyInput _mi, DeployProxyOutput _mo) public {
-        DeployProxySingleton(_mi, _mo);
-        _mo.checkOutput(_mi);
+        deployProxySingleton(_mi, _mo);
+        checkOutput(_mi, _mo);
     }
 
-    function DeployProxySingleton(DeployProxyInput _mi, DeployProxyOutput _mo) internal {
+    function deployProxySingleton(DeployProxyInput _mi, DeployProxyOutput _mo) internal {
         address owner = _mi.owner();
         vm.broadcast(msg.sender);
         IProxy proxy = IProxy(
@@ -88,5 +69,16 @@ contract DeployProxy is Script {
 
         vm.label(address(proxy), "Proxy");
         _mo.set(_mo.proxy.selector, address(proxy));
+    }
+
+    function checkOutput(DeployProxyInput _mi, DeployProxyOutput _mo) public {
+        DeployUtils.assertValidContractAddress(address(_mo.proxy()));
+        IProxy prox = _mo.proxy();
+        vm.prank(_mi.owner());
+        address proxyOwner = prox.admin();
+
+        require(
+            proxyOwner == _mi.owner(), "DeployProxy: owner of proxy does not match the owner specified in the input"
+        );
     }
 }
