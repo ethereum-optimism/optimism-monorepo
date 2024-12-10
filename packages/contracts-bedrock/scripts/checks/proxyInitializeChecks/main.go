f40fbd1461a7f6e073929f2521d54887a1896f41
Michael Amadi <amadimichaeld@gmail.com>
2024-12-10 13:00:09 +0100

    fixes

--- packages/contracts-bedrock/scripts/checks/proxyInitializeChecks/main.go
+++ packages/contracts-bedrock/scripts/checks/proxyInitializeChecks/main.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
 
 	"github.com/ethereum-optimism/optimism/op-chain-ops/solc"
 	"github.com/ethereum-optimism/optimism/packages/contracts-bedrock/scripts/checks/common"
 )
-
-var excludeContracts = []string{"forge-artifacts/OptimismPortalInterop.sol/OptimismPortalInterop.json", "forge-artifacts/SystemConfigInterop.sol/SystemConfigInterop.json"}
 
 func main() {
 	files, err := common.FindFiles([]string{"forge-artifacts/**/*.json"}, nil)
@@ -21,7 +18,8 @@ func main() {
 	}
 
 	for _, path := range files {
-		if slices.Contains(excludeContracts, path) {
+		// skip interop labeled contracts since they're to be merged with the main contract (which they inherit) before deployment
+		if strings.HasSuffix(filepath.Base(path), "Interop.json") {
 			continue
 		}
 
@@ -75,6 +73,7 @@ func checkIfInitializerWasDisabledInConstructor(ast *solc.Ast) bool {
 			for _, _node_ := range node.Nodes {
 				if _node_.NodeType == "FunctionDefinition" && _node_.Kind == "constructor" {
 					for _, statement := range _node_.Body.Statements {
+						// The _disableInitializers() call must be in the top level block of the constructor and not nested within any other blocks e.g a block, conditional statement, loop, etc.
 						if statement.NodeType == "ExpressionStatement" && statement.Expression.NodeType == "FunctionCall" && len(statement.Expression.Expression.ArgumentTypes) == 0 {
 							return true
 						}
