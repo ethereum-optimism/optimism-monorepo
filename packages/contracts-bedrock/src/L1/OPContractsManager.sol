1b1ace22b3a4558a301bde8803321362875bdf73
Maurelian <maurelian@protonmail.ch>
2024-12-13 14:29:26 -0500

    fix semverlock

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -114,8 +114,8 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 1.0.0-beta.25
-    string public constant version = "1.0.0-beta.25";
+    /// @custom:semver 1.0.0-beta.26
+    string public constant version = "1.0.0-beta.26";
 
     /// @notice Represents the interface version so consumers know how to decode the DeployOutput struct
     /// that's emitted in the `Deployed` event. Whenever that struct changes, a new version should be used.
