2c49120f524812d089070e5b7b79d0314010a51f
protolambda <proto@protolambda.com>
2024-12-18 20:12:51 +0100

    op-node: interop RPC with reset and derivation events

---
+++ op-service/rpc/subscription.go
@@ -0,0 +1,38 @@
+package rpc
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/event"
+	"github.com/ethereum/go-ethereum/log"
+	gethrpc "github.com/ethereum/go-ethereum/rpc"
+)
+
+func SubscribeRPC[T any](ctx context.Context, logger log.Logger, feed *event.FeedOf[T]) (*gethrpc.Subscription, error) {
+	notifier, supported := gethrpc.NotifierFromContext(ctx)
+	if !supported {
+		return &gethrpc.Subscription{}, gethrpc.ErrNotificationsUnsupported
+	}
+	logger.Info("Opening subscription via RPC")
+
+	rpcSub := notifier.CreateSubscription()
+	ch := make(chan T, 10)
+	feedSub := feed.Subscribe(ch)
+
+	go func() {
+		defer logger.Info("Closing RPC subscription")
+		defer feedSub.Unsubscribe()
+
+		select {
+		case ref := <-ch:
+			if err := notifier.Notify(rpcSub.ID, ref); err != nil {
+				logger.Warn("Failed to notify RPC subscription", "err", err)
+				return
+			}
+		case err := <-rpcSub.Err():
+			logger.Warn("RPC subscription failed", "err", err)
+		}
+	}()
+
+	return rpcSub, nil
+}
