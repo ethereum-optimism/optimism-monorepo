d2b35d34b081509d03779b97bc081300852790bd
geoknee <georgeknee@googlemail.com>
2024-12-19 16:53:33 +0000

    add test case for OPM

--- op-chain-ops/cmd/ecotone-scalar/main_test.go
+++ op-chain-ops/cmd/ecotone-scalar/main_test.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"math/big"
 	"os/exec"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func runMainWithArgs(t *testing.T, args []string) (string, error) {
@@ -37,4 +40,31 @@ func TestMain_PreEcotoneScalar(t *testing.T) {
 	if o.ScalarHex != "0x00000000000000000000000000000000000000000000000000000000000a6fe0" {
 		t.Errorf("did not find expected output: %s", output)
 	}
+}
+
+func TestMain_PostEcotoneScalar(t *testing.T) {
+	output, err := runMainWithArgs(t, []string{"-decode=452312848583266388373324160190187140051835877600158453279135543542576845931"})
+	if err != nil {
+		t.Fatalf("unexpected error: %v, output: %s", err, output)
+	}
+
+	o := new(outputTy)
+
+	err = json.Unmarshal([]byte(output), o)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &outputTy{
+		BaseFee:           5227,
+		BlobbaseFeeScalar: 1014213,
+		ScalarHex:         "0x010000000000000000000000000000000000000000000000000f79c50000146b",
+		Scalar:            new(big.Int),
+	}
+	_, ok := expected.Scalar.SetString("452312848583266388373324160190187140051835877600158453279135543542576845931", 0)
+
+	require.True(t, ok)
+
+	require.Equal(t, expected, o)
+
 }
