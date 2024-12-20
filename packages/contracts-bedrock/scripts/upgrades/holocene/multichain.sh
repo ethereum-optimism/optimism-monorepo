60d01115f54da1436668eced69b6620a95002f51
geoknee <georgeknee@googlemail.com>
2024-12-20 14:50:41 +0000

    pass proxyadmin addr

--- packages/contracts-bedrock/scripts/upgrades/holocene/multichain.sh
+++ packages/contracts-bedrock/scripts/upgrades/holocene/multichain.sh
@@ -3,16 +3,16 @@ set -e
 SUPERCHAIN="sepolia"
 
 echo "building bundle for op $SUPERCHAIN..."
-SYSTEM_CONFIG_PROXY_ADDR=0x034edD2A225f7f429A63E0f1D2084B9E0A93b538 just sys-cfg-bundle $PWD/op
+(PROXY_ADMIN_ADDR=0x189aBAAaa82DfC015A588A7dbaD6F13b1D3485Bc SYSTEM_CONFIG_PROXY_ADDR=0x034edD2A225f7f429A63E0f1D2084B9E0A93b538 just sys-cfg-bundle $PWD/op)
 
 echo "building bundle for mode $SUPERCHAIN..."
-SYSTEM_CONFIG_PROXY_ADDR=0x15cd4f6e0CE3B4832B33cB9c6f6Fe6fc246754c2 just sys-cfg-bundle $PWD/mode
+(PROXY_ADMIN_ADDR=0xE7413127F29E050Df65ac3FC9335F85bB10091AE SYSTEM_CONFIG_PROXY_ADDR=0x15cd4f6e0CE3B4832B33cB9c6f6Fe6fc246754c2 just sys-cfg-bundle $PWD/mode)
 
 echo "building bundle for metal $SUPERCHAIN..."
-SYSTEM_CONFIG_PROXY_ADDR=0x5D63A8Dc2737cE771aa4a6510D063b6Ba2c4f6F2 just sys-cfg-bundle $PWD/metal
+(PROXY_ADMIN_ADDR=0xF7Bc4b3a78C7Dd8bE9B69B3128EEB0D6776Ce18A SYSTEM_CONFIG_PROXY_ADDR=0x5D63A8Dc2737cE771aa4a6510D063b6Ba2c4f6F2 just sys-cfg-bundle $PWD/metal)
 
 echo "building bundle for zora $SUPERCHAIN..."
-SYSTEM_CONFIG_PROXY_ADDR=0xB54c7BFC223058773CF9b739cC5bd4095184Fb08 just sys-cfg-bundle $PWD/zora
+(PROXY_ADMIN_ADDR=0xE17071F4C216Eb189437fbDBCc16Bb79c4efD9c2 SYSTEM_CONFIG_PROXY_ADDR=0xB54c7BFC223058773CF9b739cC5bd4095184Fb08 just sys-cfg-bundle $PWD/zora)
 
 echo "Combining bundles into a super bundle..."
 
@@ -27,7 +27,7 @@ cat <<EOF > superbundle.json
 }
 EOF
 
-CONCATENATED_TXS=$(jq -s '.[].transactions' ./op/sys_cfg_bundle.json ./mode/sys_cfg_bundle.json ./metal/sys_cfg_bundle.json)
+CONCATENATED_TXS=$(jq -s '.[].transactions' ./op/sys_cfg_bundle.json ./mode/sys_cfg_bundle.json ./metal/sys_cfg_bundle.json ./zora/sys_cfg_bundle.json)
 CONCATENATED_TXS=$(echo "$CONCATENATED_TXS" | jq -s 'add')
 jq --argjson transactions "$CONCATENATED_TXS" '.transactions = $transactions' superbundle.json | jq '.' > temp.json && mv temp.json superbundle.json
 
