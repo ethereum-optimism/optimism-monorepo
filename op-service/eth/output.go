adc87377232b6d18e631b8adf6e036e7f85de07c
Adrian Sutton <adrian@oplabs.co>
2025-01-08 11:10:19 -0600

    Add OutputV1 for Superchain Output Root.

--- op-service/eth/output.go
+++ op-service/eth/output.go
@@ -1,6 +1,7 @@
 package eth
 
 import (
+	"encoding/binary"
 	"errors"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -26,6 +27,15 @@ var (
 	ErrInvalidOutputVersion = errors.New("invalid output version")
 
 	OutputVersionV0 = Bytes32{}
+	OutputVersionV1 = Bytes32{0x01}
+)
+
+const (
+	OutputVersionV0Len = 128
+
+	// OutputVersionV1MinLen is the minimum length of a V1 output root prior to hashing
+	// Must contain a 32 byte version, uint64 timestamp and at least one chain's output root hash
+	OutputVersionV1MinLen = 32 + 8 + 32
 )
 
 type Output interface {
@@ -47,13 +57,33 @@ func (o *OutputV0) Version() Bytes32 {
 }
 
 func (o *OutputV0) Marshal() []byte {
-	var buf [128]byte
+	var buf [OutputVersionV0Len]byte
 	version := o.Version()
 	copy(buf[:32], version[:])
 	copy(buf[32:], o.StateRoot[:])
 	copy(buf[64:], o.MessagePasserStorageRoot[:])
 	copy(buf[96:], o.BlockHash[:])
 	return buf[:]
+}
+
+type OutputV1 struct {
+	Timestamp uint64
+	Outputs   []Bytes32
+}
+
+func (o *OutputV1) Version() Bytes32 {
+	return OutputVersionV1
+}
+
+func (o *OutputV1) Marshal() []byte {
+	buf := make([]byte, 0, 40+len(o.Outputs)*32)
+	version := o.Version()
+	buf = append(buf, version[:]...)
+	buf = binary.BigEndian.AppendUint64(buf, o.Timestamp)
+	for _, o := range o.Outputs {
+		buf = append(buf, o[:]...)
+	}
+	return buf
 }
 
 // OutputRoot returns the keccak256 hash of the marshaled L2 output
@@ -71,13 +101,15 @@ func UnmarshalOutput(data []byte) (Output, error) {
 	switch ver {
 	case OutputVersionV0:
 		return unmarshalOutputV0(data)
+	case OutputVersionV1:
+		return unmarshalOutputV1(data)
 	default:
 		return nil, ErrInvalidOutputVersion
 	}
 }
 
 func unmarshalOutputV0(data []byte) (*OutputV0, error) {
-	if len(data) != 128 {
+	if len(data) != OutputVersionV0Len {
 		return nil, ErrInvalidOutput
 	}
 	var output OutputV0
@@ -85,5 +117,25 @@ func unmarshalOutputV0(data []byte) (*OutputV0, error) {
 	copy(output.StateRoot[:], data[32:64])
 	copy(output.MessagePasserStorageRoot[:], data[64:96])
 	copy(output.BlockHash[:], data[96:128])
+	return &output, nil
+}
+
+func unmarshalOutputV1(data []byte) (*OutputV1, error) {
+	// Must contain the version, timestamp and at least one output root.
+	if len(data) < OutputVersionV1MinLen {
+		return nil, ErrInvalidOutput
+	}
+	// Must contain complete chain output roots
+	if (len(data)-40)%32 != 0 {
+		return nil, ErrInvalidOutput
+	}
+	var output OutputV1
+	// data[:32] is the version
+	output.Timestamp = binary.BigEndian.Uint64(data[32:40])
+	for i := 40; i < len(data); i += 32 {
+		chainOutput := Bytes32{}
+		copy(chainOutput[:], data[i:i+32])
+		output.Outputs = append(output.Outputs, chainOutput)
+	}
 	return &output, nil
 }
--- op-service/eth/output_test.go
+++ op-service/eth/output_test.go
@@ -1,11 +1,22 @@
 package eth
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
+
+func TestUnmarshalOutput_UnknownVersion(t *testing.T) {
+	_, err := UnmarshalOutput([]byte{0: 0xA, 32: 0xA})
+	require.ErrorIs(t, err, ErrInvalidOutputVersion)
+}
+
+func TestUnmarshalOutput_TooShortForVersion(t *testing.T) {
+	_, err := UnmarshalOutput([]byte{0xA})
+	require.ErrorIs(t, err, ErrInvalidOutput)
+}
 
 func TestOutputV0Codec(t *testing.T) {
 	output := OutputV0{
@@ -19,8 +30,40 @@ func TestOutputV0Codec(t *testing.T) {
 	unmarshaledV0 := unmarshaled.(*OutputV0)
 	require.Equal(t, output, *unmarshaledV0)
 
-	_, err = UnmarshalOutput([]byte{0: 0xA, 32: 0xA})
-	require.ErrorIs(t, err, ErrInvalidOutputVersion)
 	_, err = UnmarshalOutput([]byte{64: 0xA})
 	require.ErrorIs(t, err, ErrInvalidOutput)
+}
+
+func TestOutputV1Codec(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
+		chainA := Bytes32{0x01}
+		chainB := Bytes32{0x02}
+		chainC := Bytes32{0x03}
+		output := OutputV1{
+			Timestamp: 7000,
+			Outputs:   []Bytes32{chainA, chainB, chainC},
+		}
+		marshaled := output.Marshal()
+		unmarshaled, err := UnmarshalOutput(marshaled)
+		require.NoError(t, err)
+		unmarshaledV1 := unmarshaled.(*OutputV1)
+		require.Equal(t, output, *unmarshaledV1)
+	})
+
+	t.Run("BelowMinLength", func(t *testing.T) {
+		_, err := UnmarshalOutput(append(OutputVersionV1[:], 0x01))
+		require.ErrorIs(t, err, ErrInvalidOutput)
+	})
+
+	t.Run("NoChainsIncluded", func(t *testing.T) {
+		_, err := UnmarshalOutput(binary.BigEndian.AppendUint64(OutputVersionV1[:], 134058))
+		require.ErrorIs(t, err, ErrInvalidOutput)
+	})
+
+	t.Run("PartialChainOutputRoot", func(t *testing.T) {
+		input := binary.BigEndian.AppendUint64(OutputVersionV1[:], 134058)
+		input = append(input, 0x01, 0x02, 0x03)
+		_, err := UnmarshalOutput(input)
+		require.ErrorIs(t, err, ErrInvalidOutput)
+	})
 }
