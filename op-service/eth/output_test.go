7ba644a5f2fdcc8bdac6db1d503fbee00310d1d7
Adrian Sutton <adrian@oplabs.co>
2025-01-08 18:44:37 -0600

    Separate super roots and output roots

--- op-service/eth/output_test.go
+++ op-service/eth/output_test.go
@@ -1,7 +1,6 @@
 package eth
 
 import (
-	"encoding/binary"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -32,38 +31,4 @@ func TestOutputV0Codec(t *testing.T) {
 
 	_, err = UnmarshalOutput([]byte{64: 0xA})
 	require.ErrorIs(t, err, ErrInvalidOutput)
-}
-
-func TestOutputV1Codec(t *testing.T) {
-	t.Run("Valid", func(t *testing.T) {
-		chainA := Bytes32{0x01}
-		chainB := Bytes32{0x02}
-		chainC := Bytes32{0x03}
-		output := OutputV1{
-			Timestamp: 7000,
-			Outputs:   []Bytes32{chainA, chainB, chainC},
-		}
-		marshaled := output.Marshal()
-		unmarshaled, err := UnmarshalOutput(marshaled)
-		require.NoError(t, err)
-		unmarshaledV1 := unmarshaled.(*OutputV1)
-		require.Equal(t, output, *unmarshaledV1)
-	})
-
-	t.Run("BelowMinLength", func(t *testing.T) {
-		_, err := UnmarshalOutput(append(OutputVersionV1[:], 0x01))
-		require.ErrorIs(t, err, ErrInvalidOutput)
-	})
-
-	t.Run("NoChainsIncluded", func(t *testing.T) {
-		_, err := UnmarshalOutput(binary.BigEndian.AppendUint64(OutputVersionV1[:], 134058))
-		require.ErrorIs(t, err, ErrInvalidOutput)
-	})
-
-	t.Run("PartialChainOutputRoot", func(t *testing.T) {
-		input := binary.BigEndian.AppendUint64(OutputVersionV1[:], 134058)
-		input = append(input, 0x01, 0x02, 0x03)
-		_, err := UnmarshalOutput(input)
-		require.ErrorIs(t, err, ErrInvalidOutput)
-	})
 }
---
+++ op-service/eth/superroot.go
@@ -0,0 +1,76 @@
+package eth
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+var (
+	ErrInvalidSuperRoot        = errors.New("invalid super root")
+	ErrInvalidSuperRootVersion = errors.New("invalid super root version")
+	SuperRootVersionV1         = byte(1)
+)
+
+const (
+	// SuperRootVersionV1MinLen is the minimum length of a V1 super root prior to hashing
+	// Must contain a 1 byte version, uint64 timestamp and at least one chain's output root hash
+	SuperRootVersionV1MinLen = 1 + 8 + 32
+)
+
+type Super interface {
+	Version() byte
+	Marshal() []byte
+}
+
+type SuperRootV1 struct {
+	Timestamp uint64
+	Outputs   []Bytes32
+}
+
+func (o *SuperRootV1) Version() byte {
+	return SuperRootVersionV1
+}
+
+func (o *SuperRootV1) Marshal() []byte {
+	buf := make([]byte, 0, 9+len(o.Outputs)*32)
+	version := o.Version()
+	buf = append(buf, version)
+	buf = binary.BigEndian.AppendUint64(buf, o.Timestamp)
+	for _, o := range o.Outputs {
+		buf = append(buf, o[:]...)
+	}
+	return buf
+}
+
+func UnmarshalSuperRoot(data []byte) (Super, error) {
+	if len(data) < 1 {
+		return nil, ErrInvalidSuperRoot
+	}
+	ver := data[0]
+	switch ver {
+	case SuperRootVersionV1:
+		return unmarshalSuperRootV1(data)
+	default:
+		return nil, ErrInvalidSuperRootVersion
+	}
+}
+
+func unmarshalSuperRootV1(data []byte) (*SuperRootV1, error) {
+	// Must contain the version, timestamp and at least one output root.
+	if len(data) < SuperRootVersionV1MinLen {
+		return nil, ErrInvalidSuperRoot
+	}
+	// Must contain complete chain output roots
+	if (len(data)-9)%32 != 0 {
+		return nil, ErrInvalidSuperRoot
+	}
+	var output SuperRootV1
+	// data[:32] is the version
+	output.Timestamp = binary.BigEndian.Uint64(data[1:9])
+	for i := 9; i < len(data); i += 32 {
+		chainOutput := Bytes32{}
+		copy(chainOutput[:], data[i:i+32])
+		output.Outputs = append(output.Outputs, chainOutput)
+	}
+	return &output, nil
+}
