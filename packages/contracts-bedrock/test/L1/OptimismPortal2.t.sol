cca6a3e017fc7c12bac221eaaa680bdfddd2091a
Michael Amadi <amadimichaeld@gmail.com>
2025-01-13 20:54:05 +0100

    ...

--- packages/contracts-bedrock/test/L1/OptimismPortal2.t.sol
+++ packages/contracts-bedrock/test/L1/OptimismPortal2.t.sol
@@ -36,7 +36,7 @@ contract OptimismPortal2_Test is CommonTest {
 
     /// @notice Tests that the version function returns a valid string. We avoid testing the
     ///         specific value of the string as it changes frequently.
-    function test_version_succeeds() external {
+    function test_version_succeeds() external view {
         assert(bytes(optimismPortal2.version()).length > 0);
     }
 
