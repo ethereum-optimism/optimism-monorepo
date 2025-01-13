91b512222f33812c37dfc41881b44a37cfde1300
JosepBove <josep@oplabs.co>
2025-01-13 14:01:49 +0100

    typo fix

--- packages/contracts-bedrock/test/universal/InitializablePublic.t.sol
+++ packages/contracts-bedrock/test/universal/InitializablePublic.t.sol
@@ -44,7 +44,7 @@ contract InitializablePublic_Test is Test {
         for (uint256 i; i < contracts.length; ++i) {
             bytes32 slotVal = vm.load(contracts[i].target, Constants.INITIALIZABLE_STORAGE);
             uint64 initialized = uint64(uint256(slotVal));
-            // Uint64 max means intialized and _disableInitializers called
+            // Uint64 max means initialized and _disableInitializers called
             assertEq(initialized, type(uint64).max);
         }
     }
