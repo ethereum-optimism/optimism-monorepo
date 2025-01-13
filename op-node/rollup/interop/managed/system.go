eba189d4c1a71cdcbc459d63dddd51480093a1b1
inphi <mlaw2501@gmail.com>
2025-01-13 17:07:13 -0600

    return stubbed canonical output for interop_pendingOutputV0AtTimestamp

--- op-node/rollup/interop/managed/system.go
+++ op-node/rollup/interop/managed/system.go
@@ -298,7 +298,8 @@ func (m *ManagedMode) PendingOutputV0AtTimestamp(ctx context.Context, timestamp 
 	if err != nil {
 		return nil, err
 	}
-	_ = ref
-	// TODO: return pending output in the optimistic block deposited transaction - https://github.com/ethereum-optimism/specs/pull/489
-	panic("not implemented")
+	// TODO: When appropriate, replace below with the output root preimage of an actual pending block contained in
+	// the optimistic block deposited transaction. see https://github.com/ethereum-optimism/specs/pull/489
+	// For now, we use the output at timestamp as-if it didn't contain invalid messages for happy path testing.
+	return m.l2.OutputV0AtBlock(ctx, ref.Hash)
 }
