99228abf27b173ef8fbe0cb0cd0127f227f0ad7e
Mark Tyneway <mark.tyneway@gmail.com>
2025-01-24 15:57:40 -0600

    p2p: test coverage

--- op-node/p2p/gossip.go
+++ op-node/p2p/gossip.go
@@ -308,7 +308,7 @@ func BuildBlocksValidator(log log.Logger, cfg *rollup.Config, runCfg GossipRunti
 		var envelope eth.ExecutionPayloadEnvelope
 
 		// [REJECT] if the block encoding is not valid
-		if blockVersion == eth.BlockV3 || blockVersion == eth.BlockV4 {
+		if blockVersion.HasParentBeaconBlockRoot() {
 			if err := envelope.UnmarshalSSZ(blockVersion, uint32(len(payloadBytes)), bytes.NewReader(payloadBytes)); err != nil {
 				log.Warn("invalid envelope payload", "err", err, "peer", id)
 				return pubsub.ValidationReject
