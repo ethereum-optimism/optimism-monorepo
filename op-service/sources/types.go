42f0bd87d137ac64a6643e1237c875271577acfc
George Knee <georgeknee@googlemail.com>
2025-01-28 17:48:20 +0000

    Update op-service/sources/types.go
    Co-authored-by: protolambda <proto@protolambda.com>

--- op-service/sources/types.go
+++ op-service/sources/types.go
@@ -62,7 +62,7 @@ type RPCHeader struct {
 	ParentBeaconRoot *common.Hash `json:"parentBeaconBlockRoot,omitempty"`
 
 	// RequestsHash was added by EIP-7685 and is ignored in legacy headers.
-	RequestsHash *common.Hash `json:"requestsHash" rlp:"optional"`
+	RequestsHash *common.Hash `json:"requestsHash,omitempty" rlp:"optional"`
 
 	// untrusted info included by RPC, may have to be checked
 	Hash common.Hash `json:"hash"`
