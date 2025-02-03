1c82c417fc0ead5134d67c0e0e1966d7c707af89
Julian Meyer <julian.meyer@coinbase.com>
2025-02-03 10:24:41 -0800

    Update docs

--- op-service/predeploys/eip2935.go
+++ op-service/predeploys/eip2935.go
@@ -2,13 +2,8 @@ package predeploys
 
 import "github.com/ethereum/go-ethereum/common"
 
-// EIP-4788 defines a deterministic deployment transaction that deploys the beacon-block-roots contract.
-// To embed the contract in genesis, we want the deployment-result, not the contract-creation tx input code.
-// Since the contract deployment result is deterministic and the same across every chain,
-// the bytecode can be easily verified by comparing it with chains like Goerli.
-// During deployment it does not modify any contract storage, the storage starts empty.
-// See https://goerli.etherscan.io/tx/0xdf52c2d3bbe38820fff7b5eaab3db1b91f8e1412b56497d88388fb5d4ea1fde0
-// And https://eips.ethereum.org/EIPS/eip-4788
+// EIP-2935 defines a deterministic deployment transaction that deploys the recent block hashes contract.
+// See https://eips.ethereum.org/EIPS/eip-2935
 var (
 	EIP2935ContractAddr     = common.HexToAddress("0x0F792be4B0c0cb4DAE440Ef133E90C0eCD48CCCC")
 	EIP2935ContractCode     = common.FromHex("0x3373fffffffffffffffffffffffffffffffffffffffe14604657602036036042575f35600143038111604257611fff81430311604257611fff9006545f5260205ff35b5f5ffd5b5f35611fff60014303065500")
