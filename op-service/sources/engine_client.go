77d6b7239d15a6945d5f68ec4ddf5562ddc58c7a
Julian Meyer <julian.meyer@coinbase.com>
2025-02-04 14:30:39 -0800

    Resolve merge conflicts

--- op-service/sources/engine_client.go
+++ op-service/sources/engine_client.go
@@ -105,7 +105,7 @@ func (s *EngineAPIClient) NewPayload(ctx context.Context, payload *eth.Execution
 	var err error
 	switch method := s.evp.NewPayloadVersion(uint64(payload.Timestamp)); method {
 	case eth.NewPayloadV4:
-		err = s.RPC.CallContext(execCtx, &result, string(method), payload, []common.Hash{}, parentBeaconBlockRoot, []hexutil.Bytes{})
+		err = s.RPC.CallContext(ctx, &result, string(method), payload, []common.Hash{}, parentBeaconBlockRoot, []hexutil.Bytes{})
 	case eth.NewPayloadV3:
 		err = s.RPC.CallContext(ctx, &result, string(method), payload, []common.Hash{}, parentBeaconBlockRoot)
 	case eth.NewPayloadV2:
