cce3382fdca221efd1ad04839376b0a529b30244
Maurelian <maurelian@protonmail.ch>
2025-02-04 13:50:05 -0500

    feat: bump semvers

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -143,9 +143,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 1.0.1
+    /// @custom:semver 1.0.2
     function version() public pure virtual returns (string memory) {
-        return "1.0.1";
+        return "1.0.2";
     }
 
     /// @notice Address of the SuperchainConfig contract shared by all chains.
