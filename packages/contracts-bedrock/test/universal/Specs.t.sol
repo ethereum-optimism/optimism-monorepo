124542d942ef104476cfc7624e308485901bf2e8
JosepBove <josep@oplabs.co>
2025-02-06 06:28:14 +0100

    add specs

--- packages/contracts-bedrock/test/universal/Specs.t.sol
+++ packages/contracts-bedrock/test/universal/Specs.t.sol
@@ -10,6 +10,7 @@ import { ForgeArtifacts, Abi, AbiEntry } from "scripts/libraries/ForgeArtifacts.
 
 // Interfaces
 import { IOPContractsManager } from "interfaces/L1/IOPContractsManager.sol";
+import { IOPPrestateUpdater } from "interfaces/L1/IOPPrestateUpdater.sol";
 import { IOptimismPortal2 } from "interfaces/L1/IOptimismPortal2.sol";
 import { IOptimismPortalInterop } from "interfaces/L1/IOptimismPortalInterop.sol";
 import { ISystemConfig } from "interfaces/L1/ISystemConfig.sol";
@@ -796,6 +797,13 @@ contract Specification_Test is CommonTest {
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("protocolVersions()") });
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("superchainProxyAdmin()") });
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("l1ContractsRelease()") });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.deploy.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.blueprints.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.chainIdToBatchInboxAddress.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.implementations.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.upgrade.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPContractsManager.addGameType.selector });
+        _addSpec({ _name: "OPPrestateUpdater", _sel: IOPPrestateUpdater.updatePrestate.selector });
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("isRC()") });
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("setRC(bool)") });
         _addSpec({ _name: "OPPrestateUpdater", _sel: _getSel("upgradeController()") });
