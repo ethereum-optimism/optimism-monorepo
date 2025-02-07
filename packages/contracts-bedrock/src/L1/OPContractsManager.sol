3d5ecbcf694fe0e0e53fceaca090604779b19b1b
alcueca <alberto@yield.is>
2025-02-07 15:14:15 +0000

    chore: bumping version and semver-lock so that CI sees them changing at the same time.

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -144,9 +144,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 1.2.0
+    /// @custom:semver 1.2.1
     function version() public pure virtual returns (string memory) {
-        return "1.2.0";
+        return "1.2.1";
     }
 
     /// @notice Address of the SuperchainConfig contract shared by all chains.
