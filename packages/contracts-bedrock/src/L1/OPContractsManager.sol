b100de28b1d0d84bc5c8aead98537eede08de68c
JosepBove <josep@aave.com>
2025-02-10 22:20:12 +0100

    pre-pr

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -144,9 +144,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 1.1.1
+    /// @custom:semver 1.2.1
     function version() public pure virtual returns (string memory) {
-        return "1.1.1";
+        return "1.2.1";
     }
 
     /// @notice Address of the SuperchainConfig contract shared by all chains.
