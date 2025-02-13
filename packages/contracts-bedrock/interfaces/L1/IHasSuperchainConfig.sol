6d22e0894b72eeb2e4ea0eb1c88a76ca46b2aac9
JosepBove <josep@oplabs.co>
2025-02-13 20:53:27 +0100

    rerun ci

--- packages/contracts-bedrock/interfaces/L1/IHasSuperchainConfig.sol
+++ packages/contracts-bedrock/interfaces/L1/IHasSuperchainConfig.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import { ISuperchainConfig } from "interfaces/L1/ISuperchainConfig.sol";
 
-/// @notice Generic interface for contracts that have a superchain config
+/// @notice Generic interface for contracts that have a superchain config.
 interface IHasSuperchainConfig {
-    /// @notice Retrieves the superchain config for a given contract
+    /// @notice Retrieves the superchain config for a given contract.
     function superchainConfig() external view returns (ISuperchainConfig);
 }
