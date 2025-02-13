82f3fb186988c46cb53f052f14a2e5f6c22aa918
JosepBove <josep@oplabs.co>
2025-02-13 19:11:30 +0100

    versions after rebase

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -144,9 +144,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 1.3.2
+    /// @custom:semver 1.4.0
     function version() public pure virtual returns (string memory) {
-        return "1.3.2";
+        return "1.4.0";
     }
 
     /// @notice Address of the SuperchainConfig contract shared by all chains.
