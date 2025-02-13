d053bbb674ce8de86aefdc9d219fb56762990aba
geoknee <georgeknee@googlemail.com>
2025-02-13 22:25:25 +0000

    remove dead code

--- op-e2e/e2eutils/transactions/count.go
+++ op-e2e/e2eutils/transactions/count.go
@@ -19,19 +19,3 @@ func TransactionsBySender(block *types.Block, sender common.Address) (int64, err
 	}
 	return txCount, nil
 }
-
-func TransactionsBytesBySender(block *types.Block, sender common.Address) (int64, error) {
-	blockBytes := int64(0)
-	for _, tx := range block.Transactions() {
-		signer := types.NewCancunSigner(tx.ChainId())
-		txSender, err := types.Sender(signer, tx)
-		if err != nil {
-			return 0, err
-
-		}
-		if txSender == sender {
-			blockBytes += int64(len(tx.Data()))
-		}
-	}
-	return blockBytes, nil
-}
