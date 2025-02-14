7fbd9c040f8bec318fbfaf0f66150f9a4458d67f
protolambda <proto@protolambda.com>
2025-02-14 21:50:20 +0100

    interop: recovery draft

--- op-supervisor/supervisor/backend/db/update.go
+++ op-supervisor/supervisor/backend/db/update.go
@@ -105,11 +105,6 @@ func (db *ChainsDB) initializedUpdateLocalSafe(chain eth.ChainID, source eth.Blo
 			return
 		}
 		logger.Warn("Failed to update local safe", "err", err)
-		db.emitter.Emit(superevents.LocalSafeOutOfSyncEvent{
-			ChainID: chain,
-			L1Ref:   source,
-			Err:     err,
-		})
 		return
 	}
 	logger.Info("Updated local safe DB")
