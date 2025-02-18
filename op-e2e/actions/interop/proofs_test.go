fbf7b26613b7c929666d55a0db815accb7db4f5e
inphi <mlaw2501@gmail.com>
2025-02-18 15:53:42 -0500

    restore test skip

--- op-e2e/actions/interop/proofs_test.go
+++ op-e2e/actions/interop/proofs_test.go
@@ -455,7 +455,7 @@ func TestInteropFaultProofs_CascadeInvalidBlock(gt *testing.T) {
 func TestInteropFaultProofs_MessageExpiry(gt *testing.T) {
 	t := helpers.NewDefaultTesting(gt)
 	// TODO(#14234): Check message expiry in op-supervisor
-	//t.Skip("Message expiry not yet implemented")
+	t.Skip("Message expiry not yet implemented")
 
 	system := dsl.NewInteropDSL(t)
 
