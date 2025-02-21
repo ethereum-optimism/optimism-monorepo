dbec1da52fd0d9497c269ffa2d1406295f561156
Teddy Knox <teddyknox@gmail.com>
2025-02-21 09:18:38 -0500

    Update withdrawal_root_test to rely on latest devnet-sdk API
    Update to target latest devnet-sdk version

--- kurtosis-devnet/tests/isthmus/withdrawal_root_test.go
+++ kurtosis-devnet/tests/isthmus/withdrawal_root_test.go
@@ -1,40 +1,42 @@
 package isthmus
 
 import (
-	"bytes"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	gtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
 
 	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/constants"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/system"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/systest"
+	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/testlib/validators"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/types"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
 	"github.com/ethereum-optimism/optimism/op-service/client"
 	"github.com/ethereum-optimism/optimism/op-service/predeploys"
-	"github.com/ethereum-optimism/optimism/op-service/solabi"
 	"github.com/ethereum-optimism/optimism/op-service/sources"
 	"github.com/ethereum-optimism/optimism/op-service/testlog"
+	"github.com/lmittmann/w3"
 )
 
-func withdrawalRootTestScenario(chainIdx uint64, userSentinel interface{}) systest.SystemTestFunc {
+func withdrawalRootTestScenario(chainIdx uint64, walletGetter validators.WalletGetter) systest.SystemTestFunc {
 	return func(t systest.T, sys system.System) {
 		ctx := t.Context()
 
-		chain := sys.L2(chainIdx)
+		chain, ok := sys.L2s()[0].(system.LowLevelChain)
+		require.True(t, ok)
+		chain.Client()
+
 		logger := testlog.Logger(t, log.LevelInfo)
 		logger.Info("Started test")
 
-		user := ctx.Value(userSentinel).(types.Wallet)
+		user := walletGetter(ctx)
 
 		// Sad eth clients
 		rpcCl, err := client.NewRPC(ctx, logger, chain.RPCURL())
@@ -57,6 +59,8 @@ func withdrawalRootTestScenario(chainIdx uint64, userSentinel interface{}) syste
 		require.NoError(t, err)
 		preWithdrawalsRoot := preProof.StorageHash
 
+		logger.Info("Got pre proof", "storage hash", preWithdrawalsRoot)
+
 		// check isthmus withdrawals-root in the block matches the state
 		gotPre := preBlock.WithdrawalsRoot()
 		require.NotNil(t, gotPre)
@@ -64,38 +68,71 @@ func withdrawalRootTestScenario(chainIdx uint64, userSentinel interface{}) syste
 
 		chainID := (*big.Int)(chain.ID())
 		signer := gtypes.LatestSignerForChainID(chainID)
-		priv, err := crypto.HexToECDSA(user.PrivateKey())
+		priv := user.PrivateKey()
 		require.NoError(t, err)
 
 		// construct call input, ugly but no bindings...
-		var input bytes.Buffer
-		require.NoError(t, solabi.WriteSignature(&input, []byte("initiateWithdrawal(address,uint256,bytes)")[:4])) // selector
-		require.NoError(t, solabi.WriteAddress(&input, common.Address{}))
-		require.NoError(t, solabi.WriteUint256(&input, big.NewInt(1000_000)))
-		require.NoError(t, solabi.WriteUint256(&input, big.NewInt(4+20+32+32))) // calldata offset to length data
-		require.NoError(t, solabi.WriteUint256(&input, big.NewInt(0)))          // length
+		funcInitiateWithdrawal := w3.MustNewFunc(`initiateWithdrawal(address, uint256, bytes memory)`, "")
+		args, err := funcInitiateWithdrawal.EncodeArgs(
+			common.Address{},
+			big.NewInt(1_000_000),
+			[]byte{},
+		)
+		require.NoError(t, err)
+
+		nonce, err := gethCl.PendingNonceAt(ctx, user.Address())
+		require.NoError(t, err)
 
 		// sign a tx to trigger a withdrawal (no ETH value, just a message), submit it
+		// Get current base fee to ensure transaction gets included
+		header, err := gethCl.HeaderByNumber(ctx, nil)
+		require.NoError(t, err)
+		baseFee := header.BaseFee
+
+		// Set gas tip to 2x current base fee and total fee cap to 3x base fee to ensure inclusion
+		gasTipCap := new(big.Int).Mul(baseFee, big.NewInt(2))
+		gasFeeCap := new(big.Int).Mul(baseFee, big.NewInt(3))
+
 		tx, err := gtypes.SignNewTx(priv, signer, &gtypes.DynamicFeeTx{
 			ChainID:   chainID,
-			Nonce:     0,
-			GasTipCap: big.NewInt(10),
-			GasFeeCap: big.NewInt(200),
-			Gas:       params.TxGas,
+			Nonce:     nonce,
+			GasTipCap: gasTipCap,
+			GasFeeCap: gasFeeCap,
+			Gas:       21624,
 			To:        &predeploys.L2ToL1MessagePasserAddr,
 			Value:     big.NewInt(0),
-			Data:      input.Bytes(),
+			Data:      args,
 		})
 		require.NoError(t, err, "sign tx")
-		require.NoError(t, gethCl.SendTransaction(ctx, tx))
+
+		// Try to simulate the transaction first to check for errors
+		_, err = gethCl.CallContract(ctx, ethereum.CallMsg{
+			From:      user.Address(),
+			To:        tx.To(),
+			Gas:       tx.Gas(),
+			GasPrice:  tx.GasFeeCap(),
+			GasTipCap: tx.GasTipCap(),
+			Value:     tx.Value(),
+			Data:      tx.Data(),
+		}, nil)
+		if err != nil {
+			logger.Error("Transaction simulation failed", "err", err)
+		}
+
+		err = gethCl.SendTransaction(ctx, tx)
+		require.NoError(t, err, "send tx")
 
 		// Find when the withdrawal was included
 		rec, err := wait.ForReceipt(ctx, gethCl, tx.Hash(), gtypes.ReceiptStatusSuccessful)
+		if err != nil {
+			logger.Error("Transaction was not included", "err", err)
+		}
 		require.NoError(t, err)
 
 		// Load the storage at this particular block
 		postBlockHash := rec.BlockHash
 		postProof, err := ethCl.GetProof(ctx, predeploys.L2ToL1MessagePasserAddr, nil, postBlockHash.String())
+		require.NoError(t, err, "Error getting L2ToL1MessagePasser contract proof")
 		postWithdrawalsRoot := postProof.StorageHash
 
 		// Check that the withdrawals-root changed
@@ -114,11 +151,15 @@ func withdrawalRootTestScenario(chainIdx uint64, userSentinel interface{}) syste
 }
 
 func TestWithdrawalsRoot(t *testing.T) {
-	chainIdx := uint64(0)         // We'll use the first L2 chain for this test
-	testUserMarker := &struct{}{} // Sentinel for the user context value
+	chainIdx := uint64(0) // We'll use the first L2 chain for this test
+
+	walletGetter, fundsValidator := validators.AcquireL2WalletWithFunds(
+		chainIdx,
+		types.NewBalance(big.NewInt(1.0*constants.ETH)),
+	)
 
 	systest.SystemTest(t,
-		withdrawalRootTestScenario(chainIdx, testUserMarker),
-		walletFundsValidator(chainIdx, types.NewBalance(big.NewInt(1.0*constants.ETH)), testUserMarker),
+		withdrawalRootTestScenario(chainIdx, walletGetter),
+		fundsValidator,
 	)
 }
