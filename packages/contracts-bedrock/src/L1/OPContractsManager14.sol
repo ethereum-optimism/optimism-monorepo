81fd49848411da9257d5d85774005ce24338126c
mbaxter <meredith@oplabs.co>
2025-02-27 14:06:28 -0500

    Create new OPCM contract for upgrade 14

---
+++ packages/contracts-bedrock/src/L1/OPContractsManager14.sol
@@ -0,0 +1,7 @@
+import {OPContractsManager} from "./OPContractsManager.sol";
+
+///  @title OPContractsManager14
+///  @notice Represents the new OPContractsManager for Upgrade 14
+contract OPContractsManager14 is OPContractsManager {
+    
+}
